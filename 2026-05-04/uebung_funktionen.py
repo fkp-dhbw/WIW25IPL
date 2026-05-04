d3547fd24b6e5fdf4d02d4a95c42e170eb6d57b7
Florian Panzer <dhbw@fkp.email>
2026-05-04 12:01:13 +0200

    Vorlesung vom 04.05.2026

--- 2026-05-04/uebung_funktionen.py
+++ 2026-05-04/uebung_funktionen.py
@@ -1,10 +1,13 @@
+def value(x):
+    f = x**2 - 2*x + 1 
+    print(str(x) + "\t" + str(f)) 
+
 def value_table():
     print("x\tf(x)") 
     print("------------") 
     x = 0 
-    while x <= 10: 
-        f = x**2 - 2*x + 1 
-        print(str(x) + "\t" + str(f)) 
+    while x <= 10:
+        value(x)
         x += 1 
 
 value_table()
